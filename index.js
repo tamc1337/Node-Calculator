7b037255461e209d82d2ad898fdbed78e9bc92b4
Taylor <themacnair@hotmail.com>
2022-05-18 13:16:10 -0400

    fix bracket issues, now to work on theOperator

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
+/*C:\Users\BigGameJames\Desktop\Node Calculator>node index.js
+(node:10224) Warning: To load an ES module, set "type": "module" in the package.json or use the .mjs extension.
+(Use `node --trace-warnings ...` to show where the warning was created)
+C:\Users\BigGameJames\Desktop\Node Calculator\index.js:1
+import { createRequire } from 'module';
+^^^^^^
+
+SyntaxError: Cannot use import statement outside a module
+    at Object.compileFunction (node:vm:352:18)
+    at wrapSafe (node:internal/modules/cjs/loader:1033:15)
+    at Module._compile (node:internal/modules/cjs/loader:1069:27)
+    at Object.Module._extensions..js (node:internal/modules/cjs/loader:1159:10)
+    at Module.load (node:internal/modules/cjs/loader:981:32)
+    at Function.Module._load (node:internal/modules/cjs/loader:822:12)
+    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:77:12)
+    at node:internal/main/run_main_module:17:47 */
+
 
 var readlineSync = require('readline-sync');
 
